6faeae276f4a183d0bc8ce4d59c3cf7706f3f978
Fitsum Zerihun <fitsa0909@gmail.com>
2025-09-10 23:08:46 +0300

    deleting existing file
    ...

--- src/utils/maskUsername.js
+++
@@ -1,6 +0,0 @@
-export const maskUsername = (username) => {
-  if (!username) return '';
-  const cleanUsername = username.replace('@', '');
-  if (cleanUsername.length <= 4) return `@${cleanUsername}`;
-  return `@${cleanUsername.slice(0, 3)}***${cleanUsername.slice(-1)}`;
-}; 
